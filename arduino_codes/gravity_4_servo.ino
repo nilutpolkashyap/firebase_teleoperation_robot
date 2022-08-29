0512d071028a637f6dbb2e9fd735a41732258bd5
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2022-08-29 23:47:55 +0530

    Update and rename gravity_5_servo.ino to gravity_4_servo.ino

--- arduino_codes/gravity_4_servo.ino
+++ arduino_codes/gravity_4_servo.ino
@@ -2,8 +2,8 @@
 Servo Myservo1;
 Servo Myservo2;
 Servo Myservo3;
-Servo Myservo4;
-Servo Myservo5;
+// Servo Myservo4;
+// Servo Myservo5;
 
 int pos;
 void setup()
@@ -12,8 +12,8 @@ void setup()
 Myservo1.attach(A0);
 Myservo2.attach(A1);
 Myservo3.attach(A2);
-Myservo4.attach(A3);
-Myservo5.attach(A4);
+// Myservo4.attach(A3);
+// Myservo5.attach(A4);
 }
 
 void loop()
@@ -24,8 +24,8 @@ for(pos=0;pos<=180;pos++){
 Myservo1.write(pos);
 Myservo2.write(pos);
 Myservo3.write(pos);
-Myservo4.write(pos);
-Myservo5.write(pos);
+// Myservo4.write(pos);
+// Myservo5.write(pos);
 delay(15);
 }
 //  delay(1000);
@@ -34,8 +34,8 @@ delay(15);
 Myservo1.write(pos);
 Myservo2.write(pos);
 Myservo3.write(pos);
-Myservo4.write(pos);
-Myservo5.write(pos);
+// Myservo4.write(pos);
+// Myservo5.write(pos);
 delay(15);
 }
 //  delay(1000);

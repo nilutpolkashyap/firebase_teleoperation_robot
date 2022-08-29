4b17c5dfb8c4f40476125d43e4fd07c5d91a9c1f
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2022-08-30 02:50:19 +0530

    Update gravity_shield_servo_control.ino

--- arduino_codes/gravity_shield_servo_control.ino
+++ arduino_codes/gravity_shield_servo_control.ino
@@ -2,42 +2,57 @@
 Servo Myservo1;
 Servo Myservo2;
 Servo Myservo3;
-// Servo Myservo4;
-// Servo Myservo5;
-
 int pos;
 void setup()
 {
-//Myservo.attach(3);
 Myservo1.attach(A0);
 Myservo2.attach(A1);
-Myservo3.attach(A2);
-// Myservo4.attach(A3);
-// Myservo5.attach(A4);
+Myservo3.attach(A5);
 }
 
 void loop()
 {
+//  Myservo1.write(0);
+//  Myservo2.write(0);
+//  Myservo3.write(0);
+//  delay(2000);
+//  Myservo1.write(90);
+//  Myservo2.write(90);
+//  Myservo3.write(90);
+//  delay(2000);
+//  Myservo1.write(180);
+//  Myservo2.write(180);
+//  Myservo3.write(180);
+//  delay(2000);
+
+//  Myservo3.write(0);
+//  delay(2000);
+//  Myservo3.write(90);
+//  delay(2000);
   
-  
-for(pos=0;pos<=180;pos++){
-Myservo1.write(pos);
-Myservo2.write(pos);
-Myservo3.write(pos);
-// Myservo4.write(pos);
-// Myservo5.write(pos);
-delay(15);
-}
-//  delay(1000);
+//  Myservo.write(0);
+//  delay(2000);
+//  Myservo.write(90);
+//  delay(2000);
+//  Myservo.write(180);
+//  delay(2000);
+//  
+  for(pos=0;pos<=180;pos++){
+    //Myservo.write(pos);
+    Myservo1.write(pos);
+    Myservo2.write(pos);
+    Myservo3.write(pos);
+    delay(15);
+  }
+  delay(1000);
   
   for(pos=180;pos>=0;pos--){
-Myservo1.write(pos);
-Myservo2.write(pos);
-Myservo3.write(pos);
-// Myservo4.write(pos);
-// Myservo5.write(pos);
-delay(15);
-}
-//  delay(1000);
+    //Myservo.write(pos);
+    Myservo1.write(pos);
+    Myservo2.write(pos);
+    Myservo3.write(pos);
+    delay(15);
+  }
+  delay(1000);
   
 }
